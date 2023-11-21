68cde1b005b6e7672efddbb868e8ece48dc8fa2a
Federico Cavallo <fc.cavallo@gmail.com>
2023-11-21 18:32:23 -0300

    Adding sum of logs

--- alpha_signals/likelihood.py
+++ alpha_signals/likelihood.py
@@ -71,9 +71,53 @@ class MaximumLikelihood:
         return integral_alpha_s
 
     def sum_log_alpha_plus(self, k, eta_minus, eta_plus, theta):
-        self._t_minus
-        self._t_plus
-        return 0
+        eta_0, tau_0 = self._get_tau_eta(self, eta_minus, eta_plus)
+        tau_matrix = tau_0 * np.ones([1, self._t_plus.shape[1]]).T
+        eta_matrix = eta_0 * np.ones([1, self._t_plus.shape[1]]).T
+
+        t_plus_matrix = self._t_plus.T * np.ones(
+            [1, tau_0.shape[1]]
+        )  # numero de fila es t, numero de columna es tau
+
+        tau_matrix_diff = t_plus_matrix - tau_matrix
+        tau_matrix_diff = np.where(tau_matrix_diff > 0, tau_matrix_diff, 0)
+
+        alpha_tau_matrix = eta_matrix * (np.exp(-k * tau_matrix_diff))
+        alpha_tau = np.sum(alpha_tau_matrix, axis=1)
+        # The signs seem to be wrong in the paper, keeping them anyway
+        result = np.sum(np.log(np.where(alpha_tau >= 0, alpha_tau, 0) + theta))
+        return result
+
+    def _get_tau_eta(self, eta_minus, eta_plus):
+        tau_0 = np.concatenate(
+            [
+                self._tau_0_minus,
+                self._tau_0_plus,
+            ],
+            axis=1,
+        )
+        eta_minus_vector = -np.ones([self._tau_0_minus.shape[1], 1]) * eta_minus
+        eta_plus_vector = np.ones([self._tau_0_plus.shape[1], 1]) * eta_plus
+        eta_vector = np.concatenate([eta_minus_vector, eta_plus_vector], axis=0).T
+        tau_eta = np.concatenate([tau_0, eta_vector])
+        tau_eta = tau_eta[:, tau_eta[0, :].argsort()]
+        tau_0 = tau_eta[:, tau_eta[0, :].argsort()][0:1, :]
+        eta_0 = tau_eta[:, tau_eta[0, :].argsort()][1:2, :]
+        return eta_0, tau_0
 
     def sum_log_alpha_minus(self, k, eta_minus, eta_plus, theta):
-        return 0
+        eta_0, tau_0 = self._get_tau_eta(self, eta_minus, eta_plus)
+        tau_matrix = tau_0 * np.ones([1, self._t_minus.shape[1]]).T
+        eta_matrix = eta_0 * np.ones([1, self._t_minus.shape[1]]).T
+        t_minus_matrix = self._t_minus.T * np.ones(
+            [1, tau_0.shape[1]]
+        )  # numero de fila es t, numero de columna es tau
+
+        tau_matrix_diff = t_minus_matrix - tau_matrix
+        tau_matrix_diff = np.where(tau_matrix_diff > 0, tau_matrix_diff, 0)
+
+        alpha_tau_matrix = eta_matrix * (np.exp(-k * tau_matrix_diff))
+        alpha_tau = np.sum(alpha_tau_matrix, axis=1)
+        # The signs seem to be wrong in the paper, keeping them anyway
+        result = np.sum(np.log(np.where(alpha_tau <= 0, -alpha_tau, 0) + theta))
+        return result
